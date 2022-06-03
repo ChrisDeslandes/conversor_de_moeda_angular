fd93c1e740ca819d3bf73822a7d691c5649218df
Christiano Deslandes Ribeiro <petracconi@gmail.com>
2022-06-03 18:22:33 -0300

    Front terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api_conversor_de_moeda';
+  title = 'Conversor de Moeda';
 }
