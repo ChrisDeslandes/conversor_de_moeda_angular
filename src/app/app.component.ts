5837d44ecbf4a138f9e73aadd61d3937f5abc429
Christiano Deslandes Ribeiro <petracconi@gmail.com>
2022-06-07 02:50:33 -0300

    Finalização do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Conversor de Moeda';
+
+  private cotacaoDolar = 0;
+  private cotacaoEuro = 0;
+
+  @ViewChild('reais', {static: true}) reaisElement: ElementRef;
+  @ViewChild('dolares', {static: true}) dolaresElement: ElementRef;
+  @ViewChild('euros', {static: true}) eurosElement: ElementRef;
+
+  constructor(reaisElement: ElementRef, dolaresElement: ElementRef, eurosElement: ElementRef, private http: HttpClient) {
+    this.reaisElement = reaisElement;
+    this.dolaresElement = dolaresElement;
+    this.eurosElement = eurosElement;
+    this.http = http;
+  }
+
+  ngOnInit() {
+    const url = 'https://api.hgbrasil.com/finance?key=e33a1dfe&format=json-cors';
+    this.http.get<any>(url).subscribe(data => {
+      if (data) {
+        this.cotacaoDolar = data.results.currencies.USD.buy
+        this.cotacaoEuro = data.results.currencies.EUR.buy
+      }
+      if (!(this.cotacaoEuro && this.cotacaoDolar)) {
+        alert("Aconteceu um erro na requisição... Tente novamente mais tarde!")
+      }
+    });
+  }
+
+  public converter() {
+    var reais = this.reaisElement.nativeElement.value;
+    this.dolaresElement.nativeElement.value = (reais / this.cotacaoDolar).toFixed(2).toString().replace(".",",");
+    this.eurosElement.nativeElement.value = (reais / this.cotacaoEuro).toFixed(2).toString().replace(".",",");
+  }
 }
